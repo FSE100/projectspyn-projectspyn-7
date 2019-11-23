bf274244b6dcc82a22186f51e4ca9fc65a779920
ThetSwe21 <57016548+ThetSwe21@users.noreply.github.com>
2019-11-22 17:11:55 -0700

    Add files via upload

--- All.m
+++ All.m
@@ -1,48 +1,64 @@
 global key
 InitKeyboard();
+brick.SetColorMode(1, 4); %Set Color Sensor connected on port 1 to Color Code Mode
+color_rgb = brick.ColorRGB(1); %Get Color on port 2
 while 1
     pause(0.01);
-    ,switch key
+    switch key
         case 'w' % auto
             while 1
-            while brick.UltrasonicDist(3) < 50 && brick.TouchPressed(1) == 0 
-                brick.MoveMotor('C', 50);
-                brick.MoveMotor('D', 50);
-            end
-            if brick.UltrasonicDist(3) > 70 && brick.TouchPressed(1) == 0
-                brick.MoveMotor('C', 45);
-                brick.MoveMotor('D', -10);
-                pause(1.6)
-                brick.MoveMotor('C', 50);
-                brick.MoveMotor('D', 50);
-                pause(2.5)
-            end
-            if brick.TouchPressed(1) == 1
-                brick.MoveMotor('C', -25);
-                brick.MoveMotor('D', -25);
+            while brick.UltrasonicDist(3) < 50 && brick.TouchPressed(2) == 0
+                brick.MoveMotor('C', 37);
+                brick.MoveMotor('D', 30);
+                 if (color_rgb(1)>= 90)
+                brick.StopMotor('C')
+                brick.StopMotor('D')
                 pause(2)
+                brick.MoveMotor('C', 37)
+                brick.MoveMotor('D', 30)
+                pause(2)
+                end
+            end
+            if brick.UltrasonicDist(3) > 70 && brick.TouchPressed(2) == 0
+                brick.MoveMotor('C', 40);
+                brick.MoveMotor('D', -10);
+                pause(5)
+                brick.MoveMotor('C', 37);
+                brick.MoveMotor('D', 30);
+                pause(5)
+            end
+            if brick.TouchPressed(2) == 1
+                brick.MoveMotor('C', -37);
+                brick.MoveMotor('D', -30);
+                pause(3)
+                if color_rgb(2) < color_rgb(1) + color_rgb(3) ||(color_rgb(1)+ color_rgb(2) < color_rgb(3))
+                    brick.StopMotor('D');
+                    brick.StopMotor('C');
+                    pause(50)
+                end
                 brick.MoveMotor('C', -10);
-                brick.MoveMotor('D', 45);
-                pause(2.1)
-           end
+                brick.MoveMotor('D', 40);
+                pause(5)
+                break;
+            end
                 brick.StopMotor('D');
                 brick.StopMotor('C'); 
             end
         case 'uparrow' % move forward
-            brick.MoveMotor('C', 50);
-            brick.MoveMotor('D', 50);
+            brick.MoveMotor('C', 37);
+            brick.MoveMotor('D', 30);
             
         case 'downarrow' % move backward
-            brick.MoveMotor('C', -50);
-            brick.MoveMotor('D', -50);
+            brick.MoveMotor('C', -37);
+            brick.MoveMotor('D', -30);
             
         case 'leftarrow' % turn left
-            brick.MoveMotor('C', 25);
-            brick.MoveMotor('D', 50);
+            brick.MoveMotor('C', -10);
+            brick.MoveMotor('D', 30);
             
         case 'rightarrow' % turn right
-            brick.MoveMotor('C', 50);
-            brick.MoveMotor('D', 25);
+            brick.MoveMotor('C', 40);
+            brick.MoveMotor('D', -10);
 
         case 'q' % stop moving
             brick.StopMotor('D');
@@ -50,10 +66,11 @@ while 1
             
         case 'o' % Lift up
             brick.MoveMotor('B', -1);
+            pause(0.1)
             
         case 'p' % Lift down
             brick.MoveMotor('B', 1);
-            
+            pause(0.1)
         case 'l' % Stop lift
             brick.StopMotor('B');
             
--- Colorsensor.m
+++ Colorsensor.m
@@ -1,10 +1,16 @@
-color = brick.ColorCode(2);
-brick.MoveMotor('C', 50);
-brick.MoveMotor('D', 45);
-while brick.MoveMotor('C', 50);
-      brick.MoveMotor('D', 45);
-if (color == 5) 
-end
-brick.StopMotor('C');
-brick.StopMotor('D');
+while 1
+    brick.SetColorMode(1, 2); %Set Color Sensor connected on port 1 to Color Code Mode
+    color = brick.ColorCode(1);
+    brick.MoveMotor('C', 37)
+    brick.MoveMotor('D', 30)
+    %print color of object
+
+    if (color == 5)
+        brick.StopMotor('C')
+        brick.StopMotor('D')
+        pause(3)
+        brick.MoveMotor('C', 37)
+        brick.MoveMotor('D', 30)
+        pause(2)
+    end
 end
